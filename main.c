f65abcd9583104bfbe3006c69c46cdd1c49537bf
qawl987 <shiny861216@gmail.com>
2026-03-06 17:19:54 +0000

    feat: lab4 done

--- main.c
+++ main.c
@@ -1,24 +1,83 @@
-#define UART_BASE 0x10000000UL
-#define UART_RBR  ((volatile unsigned char*)(UART_BASE + 0x0))
-#define UART_THR  ((volatile unsigned char*)(UART_BASE + 0x0))
-#define UART_LSR  ((volatile unsigned char*)(UART_BASE + 0x5))
-#define LSR_DR    (1 << 0)
-#define LSR_TDRQ  (1 << 5)
+extern char uart_getc(void);
+extern void uart_putc(char c);
+extern void uart_puts(const char* s);
+extern void uart_hex(unsigned long h);
 
-char uart_getc() {
-    while (!(*UART_LSR & LSR_DR));
-    return *UART_RBR;
+#define SBI_EXT_BASE      0x10
+
+enum sbi_ext_base_fid {
+    SBI_EXT_BASE_GET_SPEC_VERSION,
+    SBI_EXT_BASE_GET_IMP_ID,
+    SBI_EXT_BASE_GET_IMP_VERSION,
+    SBI_EXT_BASE_PROBE_EXT,
+    SBI_EXT_BASE_GET_MVENDORID,
+    SBI_EXT_BASE_GET_MARCHID,
+    SBI_EXT_BASE_GET_MIMPID,
+};
+
+struct sbiret {
+    long error;
+    long value;
+};
+
+struct sbiret sbi_ecall(int ext,
+                        int fid,
+                        unsigned long arg0,
+                        unsigned long arg1,
+                        unsigned long arg2,
+                        unsigned long arg3,
+                        unsigned long arg4,
+                        unsigned long arg5) {
+    struct sbiret ret;
+    register unsigned long a0 asm("a0") = (unsigned long)arg0;
+    register unsigned long a1 asm("a1") = (unsigned long)arg1;
+    register unsigned long a2 asm("a2") = (unsigned long)arg2;
+    register unsigned long a3 asm("a3") = (unsigned long)arg3;
+    register unsigned long a4 asm("a4") = (unsigned long)arg4;
+    register unsigned long a5 asm("a5") = (unsigned long)arg5;
+    register unsigned long a6 asm("a6") = (unsigned long)fid;
+    register unsigned long a7 asm("a7") = (unsigned long)ext;
+    asm volatile("ecall"
+                 : "+r"(a0), "+r"(a1)
+                 : "r"(a2), "r"(a3), "r"(a4), "r"(a5), "r"(a6), "r"(a7)
+                 : "memory");
+    ret.error = a0;
+    ret.value = a1;
+    return ret;
 }
 
-void uart_putc(char c) {
-    while (!(*UART_LSR & LSR_TDRQ));
-    *UART_THR = c;
-}
-
-void uart_puts(const char* s) {
-    while (*s) {
-        uart_putc(*s++);
+/**
+ * sbi_get_spec_version() - Get the SBI specification version.
+ *
+ * Return: The current SBI specification version.
+ * The minor number of the SBI specification is encoded in the low 24 bits,
+ * with the major number encoded in the next 7 bits. Bit 31 must be 0.
+ */
+long sbi_get_spec_version(void) {
+    // TODO: Implement this function
+    struct sbiret result = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_GET_SPEC_VERSION, 0, 0, 0, 0, 0, 0);
+    if (result.error) {
+        return result.error;
     }
+    return result.value;
+}
+
+long sbi_get_impl_id() {
+    // TODO: Implement this function
+    struct sbiret result = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_GET_IMP_ID, 0, 0, 0, 0, 0, 0);
+    if (result.error) {
+        return result.error;
+    }
+    return result.value;
+}
+
+long sbi_get_impl_version() {
+    // TODO: Implement this function
+    struct sbiret result = sbi_ecall(SBI_EXT_BASE, SBI_EXT_BASE_GET_IMP_VERSION, 0, 0, 0, 0, 0, 0);
+    if (result.error) {
+        return result.error;
+    }
+    return result.value;
 }
 
 int strcmp(const char *s1, const char *s2) {
@@ -76,7 +135,18 @@ void start_kernel() {
                 uart_puts("  info  - print system info.\n");
             } else if (strcmp(cmd_buf, "hello world") == 0){
                 uart_puts("Hello world.\n");
-            }  else {
+            } else if (strcmp(cmd_buf, "info") == 0){
+                uart_puts("System information:\n");
+                uart_puts("  OpenSBI specification version:");
+                uart_hex(sbi_get_spec_version());
+                uart_puts("\r\n");
+                uart_puts("  implementation ID:");
+                uart_hex(sbi_get_impl_id());
+                uart_puts("\r\n");
+                uart_puts("  implementation version:");
+                uart_hex(sbi_get_impl_version());
+                uart_puts("\r\n");
+            } else {
                 uart_puts("Unknown command: ");
                 uart_puts(cmd_buf);
                 uart_puts("\r\n");
