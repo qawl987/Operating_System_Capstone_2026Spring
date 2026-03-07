8cee168196c672c7aec844b664a813b7658fc524
qawl987 <shiny861216@gmail.com>
2026-03-07 08:02:26 +0000

    feat: on-board modification

--- Makefile
+++ Makefile
@@ -11,8 +11,22 @@ build: clean
 	$(LD) -T link.ld -o $(TARGET).elf *.o
 	$(OBJCOPY) -O binary $(TARGET).elf $(TARGET)
 
-run: build $(TARGET)
+build_pi: clean
+	$(CC) $(CFLAGS) -c *.S *.c
+	$(LD) -T link_pi.ld -o $(TARGET).elf start.o main.o uart.o
+	$(OBJCOPY) -O binary $(TARGET).elf $(TARGET).bin
+	mkimage -f kernel.its kernel.fit
+
+run: build
 	$(QEMU) -M virt -m 8G -kernel $(TARGET) -display none -serial stdio
 
+deploy: build_pi
+	@echo "開始部署 kernel.fit 到 SD 卡..."
+	sudo mkdir -p /mnt
+	sudo mount /dev/sdb1 /mnt
+	sudo cp kernel.fit /mnt/
+	sudo umount /mnt
+	@echo "部署完成！你可以安全拔除 SD 卡了。"
+
 clean:
-	rm -f $(TARGET) $(TARGET).elf *.o
+	rm -f $(TARGET) $(TARGET).bin $(TARGET).elf *.o kernel.fit
--- uart.c
+++ uart.c
@@ -1,29 +1,33 @@
-#define UART_BASE 0x10000000UL
-#define UART_RBR  (unsigned char*)(UART_BASE + 0x0)
-#define UART_THR  (unsigned char*)(UART_BASE + 0x0)
-#define UART_LSR  (unsigned char*)(UART_BASE + 0x5)
-#define LSR_DR    (1 << 0)
-#define LSR_TDRQ  (1 << 5)
+struct sbiret {
+    long error;
+    long value;
+};
+extern struct sbiret sbi_ecall(int ext, int fid, unsigned long arg0, unsigned long arg1, 
+                               unsigned long arg2, unsigned long arg3, unsigned long arg4, unsigned long arg5);
 
-char uart_getc() {
-    while ((*UART_LSR & LSR_DR) == 0)
-        ;
-    char c = (char)*UART_RBR;
+// 呼叫 OpenSBI Legacy Console Putchar (Extension ID: 1)
+void uart_putc(char c) {
+    if (c == '\n') {
+        sbi_ecall(1, 0, '\r', 0, 0, 0, 0, 0); // 處理換行對齊
+    }
+    sbi_ecall(1, 0, c, 0, 0, 0, 0, 0);
+}
+
+// 呼叫 OpenSBI Legacy Console Getchar (Extension ID: 2)
+char uart_getc(void) {
+    struct sbiret ret;
+    do {
+        ret = sbi_ecall(2, 0, 0, 0, 0, 0, 0, 0);
+    } while (ret.error == -1); // 如果回傳 -1 代表還沒有按下鍵盤，繼續等
+    
+    char c = (char)ret.error;
     return c == '\r' ? '\n' : c;
 }
 
-void uart_putc(char c) {
-    if (c == '\n')
-        uart_putc('\r');
-
-    while ((*UART_LSR & LSR_TDRQ) == 0)
-        ;
-    *UART_THR = c;
-}
-
 void uart_puts(const char* s) {
-    while (*s)
+    while (*s) {
         uart_putc(*s++);
+    }
 }
 
 void uart_hex(unsigned long h) {
